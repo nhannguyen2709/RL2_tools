19071c682332365e8d18d01ed64ea73aafbc6cdb
nhannguyen2709 <nhanthanhnguyen94@gmail.com>
2025-08-10 11:23:37 +0000

    fixed arg

--- RL2/trainer/ppo_search.py
+++ RL2/trainer/ppo_search.py
@@ -78,7 +78,8 @@ class PPOSearchTrainer(Trainer):
             )
         elif self.config.adv.estimator == "reinforce":
             compute_reinforce_adv(
-                data_list, 
+                data_list,
+                self.config.data.responses_per_prompt, 
                 self.config.adv.norm_var
             )
         else: 
